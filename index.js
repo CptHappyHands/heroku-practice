cb90ff990ee69f19bb9851a41e727bcff3ef1f12
Andrew Cummings <andrew.amalee@gmail.com>
2021-09-02 11:59:25 -0700

    deployable

--- index.js
+++ index.js
@@ -1,5 +1,19 @@
-const server = require("express")();
+const express = require("express");
 
-server.listen(5000, () => {
-  console.log("listening on port 5000");
+const server = express();
+
+const PORT = process.env.PORT || 5000;
+
+server.use(express.json());
+
+server.get("/", (req, res) => {
+  res.send(`<h1>We did it!</h1>`);
+});
+
+server.get("/api", (req, res) => {
+  res.json({ message: "Yeah, we did do it" });
+});
+
+server.listen(PORT, () => {
+  console.log(`listening on port ${PORT}`);
 });
