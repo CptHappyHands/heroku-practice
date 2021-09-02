083d4c5dcfb4680363bfc79f55693494c000ef4d
Andrew Cummings <andrew.amalee@gmail.com>
2021-09-02 12:11:13 -0700

    done for now

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
+require("dotenv").config(); // this allows to stash artificial enc cariables in a file
 const express = require("express");
 
 const server = express();
 
 const PORT = process.env.PORT || 5000;
+
+console.log(process.env.API_SECRET);
 
 server.use(express.json());
 
